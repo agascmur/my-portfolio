5c2ce0648b8653e4e6d85751b07db8e758184f91
agascmur <agascmur@gmail.com>
2024-12-14 01:29:46 +0100

    instagram problem fix, deployment 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App'; // Your main application component
-import RickRoll from './components/RickRoll'; // Adjust path as needed
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import RickRoll from './components/RickRoll';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router> {/* Using HashRouter instead of BrowserRouter */}
+    <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/rr" element={<RickRoll />} />
+        <Route path="/my-portfolio/" element={<RickRoll />} />
+        <Route path="/my-portfolio/rr" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
